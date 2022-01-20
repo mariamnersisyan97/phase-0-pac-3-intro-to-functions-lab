45d1d33f7886182b7593cb4264bb916e079a97f5
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-01-20 17:55:49 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    string = string.toUpperCase();
+    console.log(string);
+}
+function logWhisper(string) {
+    string = string.toLowerCase();
+    console.log(string);
+}
+function sayHiToHeadphonedRoommate(string) {
+    string = string.toLowerCase();
+    return `I can\'t hear you!`;
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return `I can\'t hear you!`;
+    }
+    if (string === string.toUpperCase()) {
+        return 'YES INDEED!';
+    }
+    if (string === `Let\'s have dinner together`); {
+        return `I would love to!`;
+    }
+    }
+
+    
+
+
